427db0d2e4e379ddba4a295255287dd4c9f722b0
Asif Ali <asifali.pa@simelabs.com>
2021-12-03 19:31:29 +0530

    end of section 2

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Persistence
+{
+
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set;}
+    }
+}
+    
+    
+    
